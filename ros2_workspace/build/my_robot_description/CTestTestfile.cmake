914b98d3bc7729ffec52fbf74f5a39cd6547400e
Hari Joshi <hariojoshi@gmail.com>
2024-07-02 16:51:50 +0545

    add: create ros2 workspace and add launch file

---
+++ ros2_workspace/build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/hashtag/udemy_project/ros2_workspace/src/my_robot_description
+# Build directory: /home/ubuntu/hashtag/udemy_project/ros2_workspace/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
